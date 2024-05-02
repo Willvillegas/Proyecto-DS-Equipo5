750188a732c8a005e1acb59d597329156d5595d1
EstebanPP <estebanperezp2003@gmail.com>
2024-05-02 03:57:44 -0600

    ActividadDAO

--- Server/DAOs/ActividadDAO.js
+++ Server/DAOs/ActividadDAO.js
@@ -11,67 +11,135 @@ const {ConnectionDAO}   = require('./ConnectionDAO');
 // class that represents the ActividadDAO
 class ActividadDAO{
     //method to get all the actividades from the database
-    static async getAll(){
-        const connection = new ConnectionDAO();
+    static async getAll(id){
+        const connection =  await ConnectionDAO.getInstance();
         try {
             await connection.connect();
-            const query = ''/*'SELECT * FROM Actividad'*/;
-            const result = await connection.executeQuery(query);
+            const result = await connection.executeProcedures("BuscarActividades", {
+                idPlan: id, //Id del plan
+                outCodeResult: { type: "INT", direction: "OUTPUT" }
+            });
+            console.log(result)
             return result;
         } catch (error) {
             console.log('Error getting actividades: ', error);
             throw error;
         } finally {
-            await connection.disconnect();
+            await ConnectionDAO.disconnect();
         }
     }
     //method to get an actividad by id from the database
     static async getById(id){
-        const connection = new ConnectionDAO();
+        const connection = await ConnectionDAO.getInstance();
         try {
             await connection.connect();
-            const query =''/* `SELECT * FROM Actividad WHERE id = ${id}`*/;
-            const result = await connection.executeQuery(query);
+            const result = await connection.executeProcedures("BuscarActividadId", {
+                id: id, //Id de la actividad
+                outCodeResult: { type: "INT", direction: "OUTPUT" }
+            });
+            console.log(result)
             return result;
         } catch (error) {
             console.log('Error getting actividad by id: ', error);
             throw error;
         }finally{
-            await connection.disconnect();
+            await ConnectionDAO.disconnect();
         }
     }
     //method to create an actividad in the database
     static async create(actividad){
-        const connection = new ConnectionDAO();
+        const connection = ConnectionDAO.getInstance();
         try {
             await connection.connect();
-            const query =''/*`INSERT INTO Actividad VALUES ('${actividad.nombre}', '${actividad.descripcion}', '${actividad.fecha}', '${actividad.hora}', '${actividad.lugar}', '${actividad.cupo}', '${actividad.idGuia}')`*/;
-            const result = await connection.executeQuery(query);
+            const result = await connection.executeProcedures("CrearActividad", {
+                semana: actividad.semana,
+                fecha: actividad.fecha,
+                previos: actividad.previos,
+                publicacion: actividad.publicacion,
+                recordatorios: actividad.recordatorios,
+                enlace: actividad.enlace,
+                afiche: actividad.afiche,
+                responsables: actividad.responsables,
+                tipo: actividad.tipo,
+                modalidad: actividad.modalidad,
+                idPlan: actividad.idPlan,
+                outCodeResult: { type: "INT", direction: "OUTPUT" }
+            });
+            console.log(result)
             return result;
         } catch (error) {
             console.log('Error creating actividad: ', error);
             throw error;
         }finally{
-            await connection.disconnect();
+            await ConnectionDAO.disconnect();
         }
     }
     //method to update an actividad in the database
     static async update(actividad){
-        const connection = new ConnectionDAO();
+        const connection = ConnectionDAO.getInstance();
         try {
             await connection.connect();
-            const query =''/* `UPDATE Actividad SET nombre = '${actividad.nombre}', descripcion = '${actividad.descripcion}', fecha = '${actividad.fecha}', hora = '${actividad.hora}', lugar = '${actividad.lugar}', cupo = '${actividad.cupo}', idGuia = '${actividad.idGuia}' WHERE id = ${actividad.id}`*/;
-            const result = await connection.executeQuery(query);
+            const result = await connection.executeProcedures("ModificarActividad", {
+                id: id,
+                semana: actividad.semana,
+                fecha: actividad.fecha,
+                previos: actividad.previos,
+                publicacion: actividad.publicacion,
+                recordatorios: actividad.recordatorios,
+                enlace: actividad.enlace,
+                afiche: actividad.afiche,
+                responsables: actividad.responsables,
+                tipo: actividad.tipo,
+                modalidad: actividad.modalidad,
+                outCodeResult: { type: "INT", direction: "OUTPUT" }
+            });
+            console.log(result)
             return result;
         } catch (error) {
             console.log('Error updating actividad: ', error);
             throw error;
         }finally{
-            await connection.disconnect();
+            await ConnectionDAO.disconnect();
         }
     }
     /**
      * Delete method (tobe a define in the future)
      */
+    static async delete(id, observacion){
+        const connection = await ConnectionDAO.getInstance();
+        try {
+            await connection.connect();
+            const result = await connection.executeProcedures("EliminarActividad", {
+                id: id, //Id de la actividad
+                observacion: observacion,
+                outCodeResult: { type: "INT", direction: "OUTPUT" }
+            });
+            console.log(result)
+            return result;
+        } catch (error) {
+            console.log('Error getting actividad by id: ', error);
+            throw error;
+        }finally{
+            await ConnectionDAO.disconnect();
+        }
+    }
+    static async finish(id, datos){
+        const connection = await ConnectionDAO.getInstance();
+        try {
+            await connection.connect();
+            const result = await connection.executeProcedures("FinalizarActividad", {
+                id: id, //Id de la actividad
+                dato: datos,
+                outCodeResult: { type: "INT", direction: "OUTPUT" }
+            });
+            console.log(result)
+            return result;
+        } catch (error) {
+            console.log('Error getting actividad by id: ', error);
+            throw error;
+        }finally{
+            await ConnectionDAO.disconnect();
+        }
+    }
 }
 module.exports = {ActividadDAO};
