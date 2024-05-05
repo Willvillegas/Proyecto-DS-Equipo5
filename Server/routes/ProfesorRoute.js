736be1a49d1904c0ee7392e792fd0e2c6b98591e
Willvillegas <eduardo78villegas@estudiantec.cr>
2024-05-05 03:56:32 -0600

    Refactor ProfesorRoute.js: update routes to use ProfesorController instead of MainController

--- Server/routes/ProfesorRoute.js
+++ Server/routes/ProfesorRoute.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const ProfesorRouter = express.Router();
+const ProfesorController = require('../controllers/ProfesorController');
+/* Lo anterior se excluye debido a que no se definirá un main controller 
 const MainController = require('../controllers/MainController');
-
+*/
 // Rutas para profesores
 /** TO DO: AGREGAR RUTA O MODIFICAR HANDLER*/
-ProfesorRouter.get('/', MainController.getAllProfesores);
-ProfesorRouter.get('/:id', MainController.getProfesorById);
-ProfesorRouter.put('/:id', MainController.updateProfesor);
+ProfesorRouter.get('/', ProfesorController.getAll);
+ProfesorRouter.get('/:id', ProfesorController.getById);
+//ProfesorRouter.put('/:id', null /*MainController.updateProfesor*/);
 
 module.exports = ProfesorRouter;
