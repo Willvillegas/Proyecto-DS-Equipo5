3e9d77a01e449aac8817c728be4c1b023e2d08af
Willvillegas <eduardo78villegas@estudiantec.cr>
2024-05-05 03:55:51 -0600

    feat: Add profesor route and model

--- Server/index.js
+++ Server/index.js
@@ -4,10 +4,13 @@ const express = require('express');
 const cors = require('cors');
 const  app = express();
 const cookieParser = require('cookie-parser');
+const profesorRouter = require('./routes/ProfesorRoute.js');
+const modelProfesor = require('./models/ProfesorModel.js');
 
 app.use(cors());
 app.use(json());
 app.use(cookieParser());
+app.use('/api/profesor', profesorRouter);
 
 //prueba de hola mundo
 app.get('/', (req, res) => {
@@ -18,5 +21,3 @@ const port = 3000;
 app.listen(port, () => {
     console.log(`Server is running on port http://localhost:${port}`);
 });
-
-
