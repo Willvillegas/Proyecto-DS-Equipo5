0ac3d49ff6421a826b2f7de0fd0ff5f71b70ba31
EstebanPP <estebanperezp2003@gmail.com>
2024-05-06 12:24:52 -0600

    DAO estudiante y SP

--- Server/DAOs/EstudianteDAO.js
+++ Server/DAOs/EstudianteDAO.js
@@ -5,79 +5,104 @@ const {ConnectionDAO}   = require('./ConnectionDAO');
 // class that represents the EstudianteDAO
 class EstudianteDAO{
     //method to get all the estudiantes from the database
-    static async getAll(){
-        const connection = new ConnectionDAO();
+    static async getAll(idEquipo){
+        const connection = new ConnectionDAO.getInstance();
         try {
             await connection.connect();
-            const query = 'SELECT * FROM Estudiante';
-            const result = await connection.executeQuery(query);
+            const result = await connection.executeProcedures("BuscarEstudiantes", {
+                idEquipo: idEquipo,
+                outCodeResult: { type: "INT", direction: "OUTPUT" }
+            });
             return result;
         } catch (error) {
             console.log('Error getting estudiantes: ', error);
             throw error;
         } finally {
-            await connection.disconnect();
+            await ConnectionDAO.disconnect();
         }
     }
     //method to get an estudiante by id from the database
     static async getById(id){
-        const connection = new ConnectionDAO();
+        const connection = new ConnectionDAO.getInstance();
         try {
             await connection.connect();
-            const query = `SELECT * FROM Estudiante WHERE id = ${id}`;
-            const result = await connection.executeQuery(query);
+            const result = await connection.executeProcedures("BuscarEstudianteId", {
+                id: id,
+                outCodeResult: { type: "INT", direction: "OUTPUT" }
+            });
             return result;
         } catch (error) {
             console.log('Error getting estudiante by id: ', error);
             throw error;
         }finally{
-            await connection.disconnect();
+            await ConnectionDAO.disconnect();
         }
     }
     //method to create an estudiante in the database
-    static async create(estudiante){
-        const connection = new ConnectionDAO();
+    static async create(estudiante, idEquipo){
+        const connection = new ConnectionDAO.getInstance();
         try {
             await connection.connect();
-            const query =''/*`INSERT INTO Estudiante VALUES ('${estudiante.nombre}', '${estudiante.apellido}', '${estudiante.correo}')`*/;
-            /*`INSERT INTO Estudiante VALUES ('${estudiante.carnet}', '${estudiante.nombre}', '${estudiante.apellido1}', '${estudiante.apellido2}', '${estudiante.correo}', '${estudiante.telefono}', '${estudiante.sede}', '${estudiante.estado}', '${estudiante.equipo}')`*/
-            const result = await connection.executeQuery(query);
+            const result = await connection.executeProcedures("CrearEstudiante", {
+                idEquipo: idEquipo,
+	            carnet: estudiante.carnet,
+	            correo: estudiante.correo,
+	            nombre: estudiante.nombre,
+	            apellido1: estudiante.apellido1,
+	            apellido2: estudiante.apellido2,
+	            telefono: estudiante.telefono,
+	            sede: estudiante.sede,
+                outCodeResult: { type: "INT", direction: "OUTPUT" }
+            });
+            console.log(result)
             return result;
         } catch (error) {
             console.log('Error creating estudiante: ', error);
             throw error;
         }finally{
-            await connection.disconnect();
+            await ConnectionDAO.disconnect();
         }
     }
     //method to update an estudiante in the database
-    static async update(estudiante){
-        const connection = new ConnectionDAO();
+    static async update(estudiante, id){
+        const connection = new ConnectionDAO.getInstance();
         try {
             await connection.connect();
-            const query =''/* `UPDATE Estudiante SET nombre = '${estudiante.nombre}', apellido = '${estudiante.apellido}', correo = '${estudiante.correo}' WHERE id = ${estudiante.id}`*/;
-            const result = await connection.executeQuery(query);
+            const result = await connection.executeProcedures("ModificarEstudiante", {
+                id: id,
+	            carnet: estudiante.carnet,
+	            correo: estudiante.correo,
+	            nombre: estudiante.nombre,
+	            apellido1: estudiante.apellido1,
+	            apellido2: estudiante.apellido2,
+	            telefono: estudiante.telefono,
+	            sede: estudiante.sede,
+                outCodeResult: { type: "INT", direction: "OUTPUT" }
+            });
+            console.log(result)
             return result;
         } catch (error) {
             console.log('Error updating estudiante: ', error);
             throw error;
         }finally{
-            await connection.disconnect();
+            await ConnectionDAO.disconnect();
         }
     }
     //method to delete an estudiante from the database
     static async delete(id){
-        const connection = new ConnectionDAO();
+        const connection = new ConnectionDAO.getInstance();
         try {
             await connection.connect();
-            const query =''/* `DELETE FROM Estudiante WHERE id = ${id}`*/;
-            const result = await connection.executeQuery(query);
+            const result = await connection.executeProcedures("EliminarEstudiante", {
+                id: id,
+                outCodeResult: { type: "INT", direction: "OUTPUT" }
+            });
             return result;
         } catch (error) {
             console.log('Error deleting estudiante: ', error);
             throw error;
         }finally{
-            await connection.disconnect();
+            await ConnectionDAO.disconnect();
         }
     }
 }
