78f2cefbbbdeac2c6762844648bc05eff73df87c
Willvillegas <eduardo78villegas@estudiantec.cr>
2024-05-05 22:44:19 -0600

    Update login functionality to compare passwords correctly

---
+++ Server/utils/nodemailer.js
@@ -0,0 +1,49 @@
+const mailer = require('nodemailer');
+
+class EmailSender {
+    constructor() {
+        this.transporter = mailer.createTransport({
+            host: 'smtp-mail.outlook.com',
+            port: 587,
+            secure: false,
+            auth: {
+                user: 'proyecto.ds.grupo5@hotmail.com',
+                pass: 'ProyectoDS2024'
+            }
+        });
+        this._from = 'proyecto.ds.grupo5@hotmail.com';
+    }
+
+    // Método para enviar correo de recuperación de contraseña
+    static async sendEmailRestorePassword(email, token){
+        try {
+            const sender = new EmailSender(); // Crear una instancia de EmailSender
+            await sender.transporter.sendMail({
+                from: sender._from,
+                to: email,
+                subject: 'Recuperación de contraseña',
+                text: `Para restablecer tu contraseña, ingresa el siguiente token en la aplicación (expira en 2 horas): ${token}`
+            });
+        } catch (error) {
+            console.log('Error sending email recover password: ', error);
+        }
+    }
+
+    // Método para enviar correo genérico
+    static async sendEmail(email, subject, text){
+        try {
+            const sender = new EmailSender(); // Crear una instancia de EmailSender
+            await sender.transporter.sendMail({
+                from: sender._from,
+                to: email,
+                subject: subject,
+                text: text
+            });
+        } catch (error) {
+            console.log('Error sending email: ', error);
+        }
+    }
+    /* Implementación de función para enviar correos de archivos */
+}
+
+module.exports = EmailSender;
