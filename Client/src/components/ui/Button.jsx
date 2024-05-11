43ff17e0679639881ad77e4ec042dc6c80837de3
AngelicaDiazB14 <diazbarriosangelica00@gmail.com>
2024-05-11 01:05:42 -0600

    mejora de la página LoginPage;

--- Client/src/components/ui/Button.jsx
+++ Client/src/components/ui/Button.jsx
@@ -5,7 +5,8 @@ export function Button(props) {
             className="flex w-full justify-center 
             rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 
             text-white shadow-sm hover:bg-indigo-500 focus-visible:outline 
-            focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600
+            active:scale-[.98] active:duration-75 hover:scale-[1.01]"
         {...props}
         
 
