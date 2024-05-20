b142244c07b7137464c9813adaa5bf9b9c36f5a5
EstebanPP <estebanperezp2003@gmail.com>
2024-05-20 11:45:02 -0600

    prueba email

--- Server/controllers/UsuarioController.js
+++ Server/controllers/UsuarioController.js
@@ -75,7 +75,7 @@ class UsuarioController {
             }
             const token = jwt.sign({ correo: usuario[0].correo }, 'Proyecto-DS', { expiresIn: 60 * 15 });
             //send email with token
-            emailSender.sendEmailRestorePassword(usuario[0].correo, token);
+            await emailSender.sendEmailRestorePassword(usuario[0].correo, token);
             res.status(200).json({ message: 'Token enviado al correo' ,token: token});
         } catch (error) {
             res.status(500).json({ error: 'Error al resetear la contraseña' });
