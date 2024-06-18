c8979df2803669488e6f336965593069fdf83683
AngelicaDiazB14 <diazbarriosangelica00@gmail.com>
2024-06-18 16:57:47 -0600

    YA MUESTRA LA IMAGEN DEL ESTUDIANTE CUANDO SE DESEA MODIFICARLO Y MUESTRA UNOS MENSAJES CUANDO YA SE SUBIDO LA FOTO Y CUANDO SE HAN GUARDADO CAMBIOS EN MODIFICAR ESTUDIANTE.
    NOTA: LA FOTO CUANDO SE CAMBIE NO SE VA A MOSTRAR DE UNA VEZ REFRESCADA EN LA PANTALLA DE MODIFICAR, SOLO HASTA QUE PRESIONE EL BOTÓN DE VOLVER PODRÁ VER QUE EL CAMBIO SÍ SE REALIZÓ.

--- Client/src/Pages/ModificarEstudiante.jsx
+++ Client/src/Pages/ModificarEstudiante.jsx
@@ -1,10 +1,9 @@
-
 import React, { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import API_ROOT from '../../apiRoutes';
-import ErrorMessage from '../components/ErrorMessage'; //PopUpError
-import { useAuthContext } from '../context/AuthContext'; // Importa el contexto de autenticación
+import ErrorMessage from '../components/ErrorMessage'; // PopUpError
+import { useAuthContext } from '../context/AuthContext'; 
 
 const sedeOptions = ['Cartago', 'Limon', 'San Carlos', 'Alajuela', 'San Jose'];
 
@@ -12,15 +11,14 @@ const userTypeButtons = [
   {
     text: 'Volver',
     onClick: () => console.log("Volver"),
-    roles: [1, 2, 3, 4, 5] // 1 asistente Cartago, 2 asistente sede, 3 asistente sin sede, 4 profesor que está logueado, 5 profesor guía
+    roles: [1, 2, 3, 4, 5] 
   }
 ];
 
-const Button = ({ text, onClick, key }) => (
+const Button = ({ text, onClick }) => (
   <button
     className="text-white bg-blue-500 hover:bg-blue-700 font-bold py-1 px-4 rounded mr-4 active:scale-[.98] active:duration-75 hover:scale-[1.01]"
     onClick={onClick}
-    key={key}
   >
     {text}
   </button>
@@ -42,11 +40,34 @@ function ModificarEstudiante() {
   const [modifiedEstudianteInfo, setModifiedEstudianteInfo] = useState({});
   const [isEditing, setIsEditing] = useState(false);
   const [errorMessage, setErrorMessage] = useState('');
+  const [successMessage, setSuccessMessage] = useState(''); // Nuevo estado para mensajes de éxito
   const [showModal, setShowModal] = useState(false);
   const { currentUser } = useAuthContext(); // Obtén el usuario actual del contexto de autenticación
   const [profileImage, setProfileImage] = useState(null);
+  const [imageSrc, setImageSrc] = useState('');
 
   useEffect(() => {
+    const fetchPhoto = async (idStudent) => {
+      let photoUrl = '';
+
+      if (currentUser.tipo === 5) {
+        photoUrl = `${API_ROOT}/api/estudiantes/photo/${idStudent}`;
+      } else {
+        photoUrl = `${API_ROOT}/api/estudiantes/photo/${id}`;
+      }
+
+      const response = await fetch(photoUrl, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'image/png',
+        },
+      });
+
+      const blob = await response.blob();
+      const url = URL.createObjectURL(blob);
+      setImageSrc(url);
+    };
+
     const fetchEstudiante = async () => {
       try {
         let response;
@@ -57,8 +78,10 @@ function ModificarEstudiante() {
           // Otros tipos de usuario
           response = await axios.get(`${API_ROOT}/api/estudiantes/${id}`);
         }
-        setEstudianteInfo(response.data[0]);
-        setModifiedEstudianteInfo(response.data[0]);
+        const studentData = response.data[0];
+        setEstudianteInfo(studentData);
+        setModifiedEstudianteInfo(studentData);
+        fetchPhoto(studentData.id);
       } catch (error) {
         console.error('Error al obtener la información del estudiante:', error);
       }
@@ -68,9 +91,9 @@ function ModificarEstudiante() {
   }, [id, currentUser.id, currentUser.tipo]);
 
   const handleSaveChanges = () => {
-    if (modifiedEstudianteInfo.telefono && modifiedEstudianteInfo.telefono.length!== 8) {
+    if (modifiedEstudianteInfo.telefono && modifiedEstudianteInfo.telefono.length !== 8) {
       console.log('El número de teléfono debe tener 8 dígitos.');
-      setErrorMessage('El número de teléfono debe tener 8 dígitos.'); 
+      setErrorMessage('El número de teléfono debe tener 8 dígitos.');
       return;
     }
     setShowModal(true); // Mostrar el modal cuando se haga clic en "Guardar"
@@ -80,7 +103,7 @@ function ModificarEstudiante() {
     const { name, value } = e.target;
 
     setModifiedEstudianteInfo(prevState => ({
-     ...prevState,
+      ...prevState,
       [name]: value
     }));
   };
@@ -89,64 +112,42 @@ function ModificarEstudiante() {
     setProfileImage(e.target.files[0]);
   };
 
-  // Función para confirmar y guardar los cambios
-  const handleConfirmChanges = () => {
-    axios.put(`${API_ROOT}/api/estudiantes/update/${id}`, modifiedEstudianteInfo)
-      .then(response => {
-        console.log('Cambios guardados exitosamente:', response.data);
-        console.log(modifiedEstudianteInfo);
-        setEstudianteInfo(modifiedEstudianteInfo);
-        setIsEditing(false);
-        setShowModal(false); // Ocultar el modal después de guardar los cambios
-      })
-      .catch(error => {
-        console.error('Error al guardar los cambios:', error);
-      });
-    /**
-     * Enpoint para actualizar la foto de perfil (FALTA que la BD implemente el SP para actualizar la foto de perfil)
-     */
-     
-  };
-  const handlephotoSubmit = async (e) => {
-    e.preventDefault();
-    const formData = new FormData();
-    formData.append('foto', profileImage);
-    await axios.put(`${API_ROOT}/api/estudiantes/photo/${modifiedEstudianteInfo.id}`, formData)
-      .then(response => {
-        console.log('Foto de perfil actualizada:', response.data);
-      })
-      .catch(error => {
-        console.error('Error al actualizar la foto de perfil:', error);
-      });
-  };
-
-
-  const profileButtons = [
-    {
-      text: 'Subir foto de perfil',
-      onClick: () => console.log('Subir foto de perfil'),
-      roles: [5]
+  const handleConfirmChanges = async (e) => {
+    e.preventDefault(); // Prevenir el comportamiento por defecto del formulario
+    try {
+      await axios.put(`${API_ROOT}/api/estudiantes/update/${id}`, modifiedEstudianteInfo);
+      if (profileImage) {
+        const formData = new FormData();
+        formData.append('foto', profileImage);
+        await axios.put(`${API_ROOT}/api/estudiantes/photo/${modifiedEstudianteInfo.id}`, formData);
+        setSuccessMessage('Foto de perfil actualizada correctamente.');
+      }
+      setEstudianteInfo(modifiedEstudianteInfo);
+      setIsEditing(false);
+      setShowModal(false);
+      setSuccessMessage('Cambios guardados correctamente.'); // Mostrar mensaje de éxito
+    } catch (error) {
+      console.error('Error al guardar los cambios:', error);
     }
-  ];
+  };
 
   return (
     <div className="min-h-screen bg-gray-800 text-white flex flex-col justify-center items-center">
       <div className="max-w-md w-full mt-16">
         <div className="flex justify-center items-center flex-col mb-8">
-          <div className="border border-gray-400 w-36 h-36 rounded-full mb-4"></div>
-          {currentUser.tipo === 5 &&(
-            <form onSubmit={handlephotoSubmit}  encType="multipart/form-data">
-
-            
-            <input
-              type="file"
-              onChange={handleProfileImageChange}
-              accept="image/png"
-            />
-            <button type="submit" className=" bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-6 rounded">Subir</button>
+          <div className="border border-gray-400 w-36 h-36 rounded-full mb-4">
+            {imageSrc && <img src={imageSrc} className="rounded-full h-36 w-36" alt="imagen del estudiante" />}
+          </div>
+          {currentUser.tipo === 5 && (
+            <form onSubmit={handleConfirmChanges} encType="multipart/form-data">
+              <input
+                type="file"
+                onChange={handleProfileImageChange}
+                accept="image/png"
+              />
+              <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-6 rounded">Subir</button>
             </form>
-          )}          
-          {/*<ButtonGroup buttons={profileButtons} userType={currentUser.tipo} />*/}
+          )}
         </div>
         <div className="mb-4 border-b-2 border-gray-600 w-full">
           <label htmlFor="nombre" className="font-bold text-white">Nombre:</label>
@@ -225,7 +226,7 @@ function ModificarEstudiante() {
         </div>
         <div className="flex justify-center">
           <ButtonGroup buttons={userTypeButtons.slice(1)} userType={currentUser.tipo} />
-          <ButtonGroup buttons={[{...userTypeButtons[0], onClick: navigateBack }]} userType={currentUser.tipo} />
+          <ButtonGroup buttons={[{ ...userTypeButtons[0], onClick: navigateBack }]} userType={currentUser.tipo} />
           <button onClick={handleSaveChanges} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-6 rounded ml-2">Guardar</button>
         </div>
         {errorMessage && <ErrorMessage message={errorMessage} onClose={() => setErrorMessage('')} />}
@@ -238,7 +239,7 @@ function ModificarEstudiante() {
             <p className="text-lg font-bold mb-4">¿Está seguro de que desea realizar los cambios?</p>
             <div className="flex justify-end">
               <button
-                className="text-white bg-red-500 hover:bg-red-700 font-bold py-2px-6 rounded mr-4"
+                className="text-white bg-red-500 hover:bg-red-700 font-bold py-2 px-6 rounded mr-4"
                 onClick={() => setShowModal(false)}
               >
                 Cancelar
@@ -253,7 +254,7 @@ function ModificarEstudiante() {
           </div>
         </div>
       )}
-      
+
       {errorMessage && (
         <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-gray-900 bg-opacity-50">
           <div className="bg-blue p-8 rounded">
@@ -267,9 +268,22 @@ function ModificarEstudiante() {
           </div>
         </div>
       )}
+
+      {successMessage && ( // Mostrar el mensaje de éxito
+        <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-gray-900 bg-opacity-50">
+          <div className="bg-green-500 p-8 rounded">
+            <p className="text-lg font-bold mb-4">{successMessage}</p>
+            <button
+              className="text-green-900 bg-green-300 hover:bg-green-500 font-bold py-2 px-4 rounded active:scale-[.98] active:duration-75 hover:scale-[1.01]"
+              onClick={() => setSuccessMessage('')}
+            >
+              OK
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
 
 export default ModificarEstudiante;
-
