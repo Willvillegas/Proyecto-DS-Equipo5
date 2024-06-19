fc2411353a6c671a1cfd1bb93f2b6fafc9dfa230
Willvillegas <eduardo78villegas@estudiantec.cr>
2024-06-19 01:17:01 -0600

    chore: Update API_ROOT URLs to production endpoints

--- Client/apiRoutes.js
+++ Client/apiRoutes.js
@@ -1,5 +1,5 @@
-//const API_ROOT = 'http://localhost:3000';
+const API_ROOT = 'http://localhost:3000';
 
-const API_ROOT = 'https://proyecto-ds-equipo5-api.vercel.app';
+//const API_ROOT = 'https://proyecto-ds-equipo5-api.vercel.app';
 
 export default API_ROOT;
